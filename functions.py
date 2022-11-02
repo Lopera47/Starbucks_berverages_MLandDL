8b379e9cdcb45c567bed29b2dc18a971e11a2408
Luis Lopera <lopera19892016gmail.com>
2022-11-02 09:51:25 -0500

    EDA is done

--- functions.py
+++ functions.py
@@ -1,4 +1,24 @@
-# This file is to write useful functions
+#This file is to write useful functions
 def rice(ar1):
-    x = ar1/2
-    return x
+     x = ar1/2
+     return x
+
+def detect_outliers(Datos,x): # function to detect outliers
+    Q3 = Datos[x].quantile(0.75)
+    Q1 = Datos[x].quantile(0.25)
+    IQR = Q3 - Q1
+    superior = Q3 + (1.5 * IQR)
+    inferior = Q1 - (1.5 * IQR)
+    out_sup = Datos[Datos[x] > superior].index
+    out_inf = Datos[Datos[x] < inferior].index
+    outliers = []
+    for i in out_sup:
+        outliers.append(i)
+        for j in out_inf:
+            outliers.append(j)
+    
+    size = len(outliers)
+    size2 = len(Datos[x])
+    percentage = (size/size2)*100
+
+    return f'There are {size} outliers in variable {x} ({percentage}%), and correspond to the indixes: {outliers}'
